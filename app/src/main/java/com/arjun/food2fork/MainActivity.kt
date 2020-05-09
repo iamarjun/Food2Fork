5886700c79e9db4d2b0bdf81d7efc3c61f6ffe9d
iamarjun <arjunmanoj1995@gmail.com>
2020-05-10 00:34:06 +0530

    basic setup

--- app/src/main/java/com/arjun/food2fork/MainActivity.kt
+++ app/src/main/java/com/arjun/food2fork/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.arjun.food2fork
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/arjun/food2fork/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.arjun.food2fork.util
+
+object Constants {
+
+    const val BASE_URL = "https://recipeapi.herokuapp.com/"
+}
