b45896cca412e787d8d67db7520208cd7fe8b121
iamarjun <arjunmanoj1995@gmail.com>
2020-05-29 19:18:07 +0530

    revamp splash screen

--- app/src/main/java/com/arjun/food2fork/splashScreen/SplashActivity.kt
+++ app/src/main/java/com/arjun/food2fork/splashScreen/SplashActivity.kt
@@ -1,16 +1,43 @@
 package com.arjun.food2fork.splashScreen
 
+import android.animation.Animator
 import android.content.Intent
 import android.os.Bundle
 import com.arjun.food2fork.MainActivity
 import com.arjun.food2fork.base.BaseActivity
+import com.arjun.food2fork.databinding.SplashScreenBinding
+import com.arjun.food2fork.util.viewBinding
+import timber.log.Timber
 
 class SplashActivity : BaseActivity() {
+
+    private val binding: SplashScreenBinding by viewBinding(SplashScreenBinding::inflate)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+        setContentView(binding.root)
+
+        binding.splashLogo.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {
+                Timber.d("onAnimationRepeat")
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                Timber.d("onAnimationEnd")
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+                Timber.d("onAnimationCancel")
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+                Timber.d("onAnimationCancel")
+            }
+
+        })
+
     }
 }
